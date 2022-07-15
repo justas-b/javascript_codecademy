63c12610662fe7405238ca53c0467a807bfd5c24
Justas Bauras <67663231+justas-b@users.noreply.github.com>
2022-07-15 14:50:18 +0100

    Create meal_maker.js

---
+++ meal_maker.js
@@ -0,0 +1,25 @@
+const menu = {
+  _meal: '',
+  _price: 0,
+  set meal(mealToCheck) {
+    if(typeof mealToCheck === 'string'){
+      return this._meal = mealToCheck;
+    };
+  },
+  set price(priceToCheck){
+    if(typeof priceToCheck === 'number'){
+      return this._price = priceToCheck;
+    };
+  },
+  get todaysSpecial(){
+    if(this._meal && this._price){
+      return `Today's special is ${this._meal} for £${this._price}.`;
+    } else{
+      return 'Meal or price was not set correctly!';
+    };
+  }
+};
+
+menu.meal = 'pizza';
+menu.price = 6;
+console.log(menu.todaysSpecial);
